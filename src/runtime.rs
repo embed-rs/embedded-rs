bafa63978623e614af94c75b7bba6f0909fd42ea
Marc Brinkmann <git@marcbrinkmann.de>
2016-09-23 10:01:12 +0200

    Made panic-fmt a feature.

--- src/runtime.rs
+++ src/runtime.rs
@@ -2,6 +2,7 @@
 // apparently, even with panic="abort" you need to have a panic_fmt
 // implementation
 use core::fmt;
+#[cfg(feature = "panic-fmt")]
 #[lang = "panic_fmt"]
 #[unwind]
 extern "C" fn panic_impl(_: fmt::Arguments, _: &'static str, _: u32) -> ! {
