9fc2769bc9be93984897a8725eee73f9f842776c
Philipp Oppermann <dev@phil-opp.com>
2016-10-20 16:16:25 +0200

    Remove manually created components module
    Instead, we use the autogenerated structs of svd_board now.

--- src/components/mod.rs
+++
@@ -1,5 +0,0 @@
-pub mod gpio;
-pub mod systick;
-pub mod rcc;
-pub mod pwr;
-pub mod flash;
--- src/components/rcc/ahb3_rstr.rs
+++
@@ -1,8 +0,0 @@
-//! AHB1 peripheral reset register (RCC_AHB1RSTR)
-
-bitflags! {
-    pub flags Register: u32 {
-        const FMCRST = 1,
-        const QSPIRST = 1 << 1,
-    }
-}
