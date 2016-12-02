5cf08d1a490462f4644f8f8daebcfcfc3a1315d3
Philipp Oppermann <dev@phil-opp.com>
2016-12-02 16:38:40 +0100

    Add dependency on `volatile`

--- src/base/volatile.rs
+++ src/base/volatile.rs
@@ -1,48 +1,4 @@
-use core::{ops, ptr};
-
-// consider:
-// http://stackoverflow.com/questions/35009015/how-do-i-write-to-a-memory-mapped-address-in-rust
-//           also, implement operators for Volatile?
-
-// mem::swap: requires immovable trait or volatile stuff ("future work")
-
-// many thanks to talchas from rust irc
-
-#[repr(C)]
-pub struct Volatile<T>(T);
-
-impl<T> Volatile<T> {
-    #[inline(always)]
-    pub fn read(&self) -> T {
-        unsafe { ptr::read_volatile(&self.0 as *const T) }
-    }
-
-    #[inline(always)]
-    pub fn write(&mut self, src: T) {
-        unsafe { ptr::write_volatile(&mut self.0, src) }
-    }
-
-    /// Updates the register value
-    #[inline(always)]
-    pub fn update<F>(&mut self, f: F)
-        where F: FnOnce(&mut T)
-    {
-        let mut value = self.read();
-        f(&mut value);
-        self.write(value);
-    }
-}
-
-impl<T> ops::BitOrAssign<T> for Volatile<T>
-    where T: ops::BitOr<T, Output = T>
-{
-    #[inline(always)]
-    fn bitor_assign(&mut self, val: T) {
-        let tmp = self.read();
-        let new_val = tmp | val;
-        self.write(new_val);
-    }
-}
+pub use volatile::Volatile;
 
 pub trait VolatileStruct: Sized {
     #[inline(always)]
